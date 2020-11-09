8d986e3ea3914ea12ff12ff0f30e195844b71364
YourPalNicky <43487424+YourPalNicky@users.noreply.github.com>
2020-11-09 04:46:37 -0500

    main

--- utils/handler_manager.js
+++ utils/handler_manager.js
@@ -3,13 +3,13 @@ module.exports = function (client) {
 	readdirSync("./commands/").forEach(file => {
 		if (!file.endsWith(".js")) return;
 		let fileName = file.split(".")[0];
-		client.commands.set(fileName, require(`./commands/${fileName}`));
+		client.commands.set(fileName, require(`../commands/${fileName}`));
 		console.log(`Loaded the ${fileName} Command!`);
 	});
 	readdirSync("./events/").forEach(file => {
 		if (!file.endsWith(".js")) return;
 		let fileName = file.split(".")[0];
-		client.on(fileName, require(`./events/${fileName}`).bind(null, client));
+		client.on(fileName, require(`../events/${fileName}`).bind(null, client));
 		console.log(`Loaded the ${fileName} Event!`);
 	});
 };
