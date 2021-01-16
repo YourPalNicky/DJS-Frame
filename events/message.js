01e50305bc4188653aea0e24cdef00c484092d5d
YourPalNicky <43487424+YourPalNicky@users.noreply.github.com>
2021-01-16 01:35:22 -0600

    added try/catch

--- events/message.js
+++ events/message.js
@@ -1,13 +1,17 @@
 module.exports = function (client, message) {
-	if (message.author.bot || !message.guild) return;
-	if (!message.content.startsWith(client.config.prefix)) return;
-
-	let command_name = message.content.toLowerCase().slice(client.config.prefix.length).trim().split(" ")[0];
-
-	if (!client.commands.has(command_name)) return;
-	
-	client.cooldown.add(message.author.id);
-	setTimeout(() => { client.cooldown.delete(message.author.id) }, client.config.cooldown * 1000);
-
-	client.commands.get(command_name)(client, message);
+    try {
+        if (message.author.bot || !message.guild) return;
+        if (!message.content.startsWith(client.config.prefix)) return;
+    
+        let command_name = message.content.toLowerCase().slice(client.config.prefix.length).trim().split(" ")[0];
+    
+        if (!client.commands.has(command_name)) return;
+        
+        client.cooldown.add(message.author.id);
+        setTimeout(() => { client.cooldown.delete(message.author.id) }, client.config.cooldown * 1000);
+    
+        client.commands.get(command_name)(client, message);
+    } catch (exception) {
+        console.log(exception);
+    };
 };
